8765b905923a9786746c589b39da835d248d3bf4
sathish <medudhula.sathish@bsitsoftware.com>
2023-01-31 15:13:09 +0530

    django authentication

--- firstproject/settings.py
+++ firstproject/settings.py
@@ -128,6 +128,18 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'sathish995sunny@gmail.com'
+EMAIL_HOST_PASSWORD = 'wmhugvcgutnkeibm'
+EMAIL_USE_TLS = True
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
